5a36d66f6bc270ffcf6c2887b5e8917d7d06d87a
hari0205 <nhariharasudhan7@gmail.com>
2023-02-03 18:01:28 +0530

    Added approve all with Salt

--- api/v1/matic/approveAll/approveall.go
+++ api/v1/matic/approveAll/approveall.go
@@ -18,7 +18,7 @@ import (
 func ApplyRoutes(r *gin.RouterGroup) {
 	g := r.Group("/approve-all")
 	{
-		g.POST("", approveAll)
+		g.POST("", approveAllWithSalt)
 	}
 }
 
@@ -64,4 +64,23 @@ func sendSuccessResponse(c *gin.Context, hash string, userId string) {
 		logo.Errorf("failed to add transaction hash: %v to user id: %v, error: %s", hash, userId, err)
 	}
 	httpo.NewSuccessResponse(200, "trasaction initiated", payload).SendD(c)
+}
+
+func approveAllWithSalt(c *gin.Context) {
+	network := "matic"
+	var req ApproveAllWithSalt
+	if err := c.ShouldBindJSON(&req); err != nil {
+		logo.Errorf("invalid request %s", err)
+		httpo.NewErrorResponse(http.StatusBadRequest, "body is invalid").SendD(c)
+		return
+	}
+
+	hash, err := polygon.SetAprovalForAllErc721(req.Mnemonic, common.HexToAddress(req.OperatorAddress), common.HexToAddress(req.ContractAddress), req.Approved)
+	if err != nil {
+		httpo.NewErrorResponse(http.StatusInternalServerError, "failed to approve all").SendD(c)
+		logo.Errorf("failed to approve all to operator: %v from wallet of userId: %v, network: %v, contractAddr: %v, error: %s", req.OperatorAddress,
+			req.UserId, network, req.ContractAddress, err)
+		return
+	}
+	sendSuccessResponse(c, hash, req.UserId)
 }
--- api/v1/matic/approveAll/types.go
+++ api/v1/matic/approveAll/types.go
@@ -10,3 +10,11 @@ type ApproveAllRequest struct {
 type TransferPayload struct {
 	TrasactionHash string
 }
+
+type ApproveAllWithSalt struct {
+	UserId          string `json:"userId" binding:"required"`
+	OperatorAddress string `json:"operatorAddress" binding:"required"`
+	Mnemonic        string `json:"mnemonic" binding:"required"`
+	ContractAddress string `json:"contractAddress" binding:"required"`
+	Approved        bool   `json:"approved" binding:"required"`
+}
